a4b369bed7e9257b6df358a9b0ca6d8a33c8af57
Huferr <hugoferreirait@gmail.com>
2021-07-29 18:04:05 -0300

    Setting globalstyles

---
+++ src/styles/globalstyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+        body, html {
+            width: 100vw;
+            height: 100vh;
+        }
+    }
+
+`;
